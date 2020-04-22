ecfee6eb6be3fa64d1aea3e2d29b2ea58ef2e8b5
liwei <250275079@qq.com>
2020-04-22 22:07:37 +0800

    ~

---
+++ src/main/java/com/blog/config/MediaProperties.java
@@ -0,0 +1,52 @@
+package com.blog.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * 磁盘媒体文件目录格式;win:‘F:\\xxx\\xxx’,lin:‘/xxx/xxx’
+ * @description: none
+ * @git: https://github.com/VictorLeeFC
+ * @date: 2020/4/21 0:30
+ * @author: li
+ * @version: v0.1
+ */
+
+@ConfigurationProperties(prefix = "media.paths")
+public class MediaProperties {
+    /** 磁盘音乐目录win:‘F:\\xxx\\xxx’,lin:‘/xxx/xxx’ */
+    private String musicPath = "";
+    /** 磁盘图片目录win:‘F:\\xxx\\xxx’,lin:‘/xxx/xxx’ */
+    private String imagePath = "";
+
+    public MediaProperties(String musicPath, String imagePath) {
+        this.musicPath = musicPath;
+        this.imagePath = imagePath;
+    }
+
+    public MediaProperties() {
+    }
+
+    public String getMusicPath() {
+        return musicPath;
+    }
+
+    public void setMusicPath(String musicPath) {
+        this.musicPath = musicPath;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    @Override
+    public String toString() {
+        return "MediaProperties{" +
+                "musicPath='" + musicPath + '\'' +
+                ", imagePath='" + imagePath + '\'' +
+                '}';
+    }
+}
